8ea5b7f27f57aab9a45cf9af4ca32a3a1cb4b44d
Ali Aristanov <aristanovali618@gmail.com>
2022-08-04 13:28:54 +0500

    create sql

---
+++ migrations/20220803140227_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.users (first_name,last_name,email,"password",access_level,created_at,updated_at) VALUES
+	 ('Ali','Aristanov','admin@admin.com','$2a$12$DdU3RO6DUavcovT.nIlbduCEx.ypx5ZlsKont8C.Gau/LYjJh1SZa',3,'2022-08-01 00:00:00','2022-08-01 00:00:00');
